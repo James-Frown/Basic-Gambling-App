25b9a1df83f62fd7a4f24ad39f164a8397fab06f
James-Frown <21122@ctca.co.za>
2023-05-18 21:03:57 +0200

    Latest Changes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,18 +1,13 @@
+// importing game filter
+
+
+// importing styling for app
 import './App.css'
-import CoinFlipCard from "./components/templates/CoinlFlipCard";
-import DiceRollCard from './components/templates/DiceRollCard';
-import CardsCard from './components/templates/CardsCard';
-import MinerCard from './components/templates/MinerCard';
-import BlackJack from './components/templates/BlackJack';
 
 function App() {
   return (
     <>
-      <CoinFlipCard />
-      <DiceRollCard />
-      <CardsCard />
-      <MinerCard />
-      <BlackJack />
+      
     </>
   );
 }
