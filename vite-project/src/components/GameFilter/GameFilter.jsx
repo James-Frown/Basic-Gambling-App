fb37b6d9703c0c37e96dcbf8fd13e194f2bcf28b
James-Frown <21122@ctca.co.za>
2023-05-18 21:21:59 +0200

    Latest Changes

--- vite-project/src/components/GameFilter/GameFilter.jsx
+++ vite-project/src/components/GameFilter/GameFilter.jsx
@@ -1,12 +1,35 @@
 // importing the game pages
 
+// import the react tools
+import { useState } from 'react';
+
 // import the styling
 import './GameFilter.css';
 
 const GameFilter = () => {
+
+    // create the states for the game filter
+    function [content, setContent] = useState();
+
     return (
         <>
-
+            <div className='games_wrapper'>
+                <div className='heading__wrapper'>
+                    <h1 className='games__heading'>
+                        Games
+                    </h1>
+                    <p className='games__description'>
+                        Here are a few games that i offer:
+                    </p>
+                </div>
+                <div className='button__wrapper'>
+                    <button className='button__selector'>
+                    </button>
+                </div>
+                <div className='content__wrapper'>
+                    {content}
+                </div>
+            </div>
         </>
     );
 };
